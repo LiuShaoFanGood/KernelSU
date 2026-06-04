53031bb9f8681f1dbd8f97b5755c47f888d8e8b5
KOWX712 <leecc0503@gmail.com>
2026-06-04 10:40:49 +0800

    manager: more customization
    * manager: bring back classic home ui
    * manager: add switch icon toggle
    * manager: add scroll/fade animation switcher
    
    Signed-off-by: KOWX712 <leecc0503@gmail.com>

--- manager/app/src/main/java/me/weishu/kernelsu/ui/screen/home/HomeMaterial.kt
+++ manager/app/src/main/java/me/weishu/kernelsu/ui/screen/home/HomeMaterial.kt
@@ -67,7 +67,10 @@ import me.weishu.kernelsu.ui.component.dialog.rememberConfirmDialog
 import me.weishu.kernelsu.ui.component.material.TonalCard
 import me.weishu.kernelsu.ui.component.rebootlistpopup.RebootListPopup
 import me.weishu.kernelsu.ui.component.statustag.StatusTag
+import me.weishu.kernelsu.ui.theme.LocalClassicUi
 import me.weishu.kernelsu.ui.theme.LocalEnableOfficialLauncher
+import me.weishu.kernelsu.ui.util.getModuleCount
+import me.weishu.kernelsu.ui.util.getSuperuserCount
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -188,6 +191,7 @@ private fun StatusCard(
     state: HomeUiState,
     actions: HomeActions,
 ) {
+    val classicUi = LocalClassicUi.current
     Column(verticalArrangement = Arrangement.spacedBy(16.dp)) {
         TonalCard(
             containerColor = if (state.ksuVersion != null) {
@@ -252,6 +256,19 @@ private fun StatusCard(
                                 text = stringResource(R.string.home_working_version, state.ksuVersion),
                                 style = MaterialTheme.typography.bodyMedium
                             )
+                            if (classicUi) {
+                                Spacer(Modifier.height(4.dp))
+                                Text(
+                                    text = stringResource(
+                                        R.string.home_superuser_count, getSuperuserCount()
+                                    ), style = MaterialTheme.typography.bodyMedium
+                                )
+                                Spacer(Modifier.height(4.dp))
+                                Text(
+                                    text = stringResource(R.string.home_module_count, getModuleCount()),
+                                    style = MaterialTheme.typography.bodyMedium
+                                )
+                            }
                         }
                     }
 
@@ -302,7 +319,7 @@ private fun StatusCard(
                 }
             }
         }
-        if (state.isFullFeatured) {
+        if (state.isFullFeatured && !classicUi) {
             Row(
                 modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.spacedBy(16.dp)
@@ -448,6 +465,7 @@ private fun DonateCard(onOpenUrl: (String) -> Unit) {
 @Composable
 private fun InfoCard(systemInfo: SystemInfo) {
     val isOfficial = LocalEnableOfficialLauncher.current
+    val isClassicUi = LocalClassicUi.current
 
     TonalCard {
         Column(
@@ -464,8 +482,10 @@ private fun InfoCard(systemInfo: SystemInfo) {
                 Row(
                     verticalAlignment = Alignment.CenterVertically
                 ) {
-                    icon()
-                    Spacer(Modifier.width(16.dp))
+                    if (!isClassicUi) {
+                        icon()
+                        Spacer(Modifier.width(16.dp))
+                    }
                     Column {
                         Text(text = label, style = MaterialTheme.typography.bodyLarge)
                         Text(
@@ -611,6 +631,7 @@ private fun HomeScreenPreviewContent(
     superuserCount: Int = 0,
     moduleCount: Int = 0,
     selinuxStatus: String = "Enforcing",
+    classicUi: Boolean = false,
 ) {
     CompositionLocalProvider(LocalUriHandler provides previewUriHandler) {
         Column(
@@ -627,6 +648,7 @@ private fun HomeScreenPreviewContent(
                     superuserCount = superuserCount,
                     moduleCount = moduleCount,
                     selinuxStatus = selinuxStatus,
+                    classicUi = classicUi,
                 ),
                 actions = actions
             )
@@ -669,8 +691,10 @@ private fun previewHomeScreenState(
     superuserCount: Int = 0,
     moduleCount: Int = 0,
     selinuxStatus: String = "Enforcing",
+    classicUi: Boolean = false,
 ) = HomeUiState(
     appName = "KernelSU",
+    classicUi = classicUi,
     kernelVersion = KernelVersion(6, 1, 0),
     ksuVersion = ksuVersion,
     lkmMode = lkmMode,
